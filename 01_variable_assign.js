fb054b4263229f347b7781a11b0b5bac89effd86
rishikeshinde1 <01rishikesh.shinde@gmail.com>
2023-02-16 12:58:21 +0530

    variable declaration

---
+++ 01_variable_assign.js
@@ -0,0 +1,26 @@
+/* Varible declaration */
+
+console.log("********----#1----********");
+var myFullName;
+console.log(myFullName);
+myFullName = "Rishikesh Shinde";
+console.log(myFullName);
+myFullName = "Rishikesh Baburao Shinde";
+console.log(myFullName);
+console.log("");
+
+
+console.log("********----#2----********");
+var myLove ="I love only JS";
+console.log(myLove);
+console.log("");
+
+console.log("********----#3----********");
+var firstName ="Rishikesh";
+var lastName = "Shinde";
+var age = "32";
+var address = "Wai Satara";
+var pincode ="415515";
+var gender ="Male";
+var isMarried ="Yes";
+console.log("First Name-", firstName, "Last Name-", lastName, "Age-", age, "Address-", address, "Pincode-", pincode, "Gender-", gender, "Is Married-", isMarried);
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,11 @@
+// Personal Details
 console.log('Rishi');
+console.log(415515);
+console.log("Wai");
+
+
+var fullName = "Rishikesh shinde";
+console.log(fullName);
+var city;
+// city = "Satara";
+console.log(city);
