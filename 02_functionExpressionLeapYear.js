6970ba6c7ab99fabb9bd2b8e24a3233b3289aadf
rishikeshinde1 <01rishikesh.shinde@gmail.com>
2023-02-28 22:35:38 +0530

    update leapYear

--- 02_functionExpressionLeapYear.js
+++ 02_functionExpressionLeapYear.js
@@ -5,6 +5,8 @@ var checkLeapYear = function (year) {
     console.log(
       `Your have provided "${year}" is not valid year value to check!`
     );
+  } else if (typeof year == "number" && year.toString().length != 4) {
+    console.log(`year value "${year}" is not valid year value to check!`);
   } else if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
     return console.log(`Year ${year} is a Leap Year!`);
   } else {
@@ -21,3 +23,4 @@ checkLeapYear("Twenty Twenty");
 checkLeapYear(undefined);
 checkLeapYear(NaN);
 checkLeapYear(1750);
+checkLeapYear(750);
